272b7ba36f12ba1c4b30bf0d4f552bbc45acddc8
vvspl <vladpolt@gmail.com>
2021-08-07 17:23:28 +0300

    redux

--- Lesson1_Context/Task2_UserMenu/src/App.jsx
+++ Lesson1_Context/Task2_UserMenu/src/App.jsx
@@ -4,13 +4,15 @@ import { DataContext } from './data-context';
 
 class App extends React.Component {
   state = {
-    name: 'Nikola Tesla',
-    avatar_url: 'https://avatars3.githubusercontent.com/u10001',
+    userData: {
+      name: 'Nikola Tesla',
+      avatar_url: 'https://avatars3.githubusercontent.com/u10001',
+    },
   };
   render() {
     return (
       <div className="page">
-        <DataContext.Provider value={this.state}>
+        <DataContext.Provider value={this.state.userData}>
           <Header />
         </DataContext.Provider>
       </div>
--- Lesson1_Context/Task2_UserMenu/src/UserMenu.jsx
+++ Lesson1_Context/Task2_UserMenu/src/UserMenu.jsx
@@ -3,7 +3,6 @@ import { DataContext } from './data-context.js';
 
 class UserMenu extends React.Component {
   render() {
-    console.log('this', this.state);
     return ( 
           <div className="menu">
             <span className="menu__greeting">{`Hello, ${this.context.name}`}</span>
