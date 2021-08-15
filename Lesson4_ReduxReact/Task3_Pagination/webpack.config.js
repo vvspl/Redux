d8377327e1d264451fb60546a91ec2ee6d465992
vvspl <vladpolt@gmail.com>
2021-08-15 16:55:06 +0300

    redux

--- Lesson4_ReduxReact/Task3_Pagination/webpack.config.js
+++ Lesson4_ReduxReact/Task3_Pagination/webpack.config.js
@@ -6,7 +6,7 @@ const webpack = require("webpack");
 module.exports = (env, argv) => {
   const isProduction = argv.mode === "production";
   const config = {
-    entry: "./src/App.jsx",
+    entry: "./src/index.jsx",
     output: {
       filename: "bundle.js"
     },
